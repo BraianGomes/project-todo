95846a3253e51ec13a1c59c9b4679db345fed052
Braian de Carvalho Gomes <gomes.braian88@gmail.com>
2023-10-19 10:04:08 +1100

    updating

--- script.js
+++ script.js
@@ -1,39 +1,72 @@
-let inputTask = document.getElementById('input-task');
-let btnAdd = document.getElementById('btn-add');
-let main = document.getElementById('list');
+let contador = 0;
+let inputTask = document.getElementById("input-task");
+let btnAdd = document.getElementById("btn-add");
+let main = document.getElementById("list");
 
-function addTask(){
-    let valorInput = inputTask.value;
+function addTask() {
+  let valorInput = inputTask.value;
 
-    if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)){
-     
-        let = novoItem = `<div class="item">
-        <div class="item-icon">
-            <span class="material-symbols-outlined">radio_button_unchecked</span>
+  if (valorInput !== "" && valorInput !== null && valorInput !== undefined) {
+    ++contador;
+
+    let = novoItem = `<div id="${contador}" class="item checked">
+        <div onclick="markTask(${contador})" class="item-icon">
+            <span id=icon_${contador}"    class="material-symbols-outlined">radio_button_unchecked</span>
         </div>
 
-        <div class="item-name">
+        <div onclick="markTask(${contador})" class="item-name">
             ${valorInput}
         </div>
 
         <div class="item-btn">
-            <button id="delete">
+            <button onclick = "del(${contador})"  class="delete">
 <span class="material-symbols-outlined">delete<span> Deletar</button>
         </div>
     </div>`;
 
     main.innerHTML += novoItem;
 
-    input.value = "";
-    input.focus();
-
-    }
+    inputTask.value = "";
+    inputTask.focus();
+  }
 }
 
-input.addEventListener("keyup", function(event){
-    
-    if(event.keycode ===13){
-        event.preventDefault();
-        btnAdd.click();
+function del(id) {
+  var task = document.getElementById(id);
+  task.remove();
+}
+
+function markTask(id){
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+
+    if(classe="item"){
+        item.classList.add('checked');
+
+        var icon = 
+        document.getElementById('icon_' + 
+        icon.classList.remove('"material-symbols-outlined">radio_button_unchecked'));
+
+        icon.classList.add('"material-symbols-outlined">check_circle');
+        
+        item.parentNode.appendChild(item);
+        
+    } else{
+
+        item.classList.add('checked');
+
+        var icon = 
+        document.getElementById('icon_' + 
+        icon.classList.remove('"material-symbols-outlined">check_circle'));
+
+        icon.classList.add('"material-symbols-outlined">radio_button_unchecked');
     }
-})
+
+}
+
+inputTask.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    btnAdd.click();
+  }
+});
